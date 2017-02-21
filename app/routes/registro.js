138621cbdbb005d6206be2c2f088ed81474ff743
bonillab <nestorbonillabrenes@gmail.com>
2017-02-21 17:48:54 -0600

    agregando registros desde json

--- app/routes/registro.js
+++ app/routes/registro.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+    return [{
+      nombre: 'Nestor Brenes',
+      cedula: 'xxxxxxxx',
+      org: 'FZT',
+      area: 'CI'
+  	}];
+}
+    
 });
