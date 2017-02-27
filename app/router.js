f40d4e55f3b7303147044892457b27a91d81747e
bonillab <nestorbonillabrenes@gmail.com>
2017-02-27 16:08:34 -0600

    agregando registros desde inputs

--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('registro');
+	this.route('registro');
 });
 
 export default Router;
--- app/routes/registro.js
+++ app/routes/registro.js
@@ -1,13 +1,44 @@
 import Ember from 'ember';
+var reg;
 
 export default Ember.Route.extend({
-	model() {
-    return [{
-      nombre: 'Nestor Brenes',
-      cedula: 'xxxxxxxx',
-      org: 'FZT',
-      area: 'CI'
-  	}];
-}
-    
+    model() {
+       reg = [{
+        nombre: 'Nestor Brenes',
+        cedula: 'xxxxxxxx',
+        org: 'FZT',
+        area: 'CI'},
+        {
+        nombre: 'Nestor Bonilla',
+        cedula: 'xxxxxxxx',
+        org: 'FZT',
+        area: 'CI'},
+        {
+        nombre: 'Alice',
+        cedula: 'xxxxxxxx',
+        org: 'FZT',
+        area: 'SP'
+    }];
+
+    return reg;    
+},
+
+    actions: {
+        click: function(){
+            var name = $('#name').val();
+            var ced = $('#ced').val();
+            var org = $('#org').val();
+            var area = $('#area').val();
+            var obj = {
+                nombre : name,
+                cedula : ced,
+                org : org,
+                area : area
+            }
+            reg.addObject(obj);
+            console.log(obj);
+      }
+    }
 });
+
+
