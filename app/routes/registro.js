25f67ab588e3e9c31f92f5e6be67917992b093a3
Samuel Gutierrez <search.sama@gmail.com>
2017-02-28 04:45:19 -0600

    detalles de apariencia grafica y de codigo

--- app/routes/registro.js
+++ app/routes/registro.js
@@ -1,43 +1,76 @@
 import Ember from 'ember';
-var reg;
+import moment from 'moment';
+
+var registro;
+var elementos = [
+    'nombre',
+    'cedula',
+    'org',
+    'area'
+];
 
 export default Ember.Route.extend({
     model() {
-       reg = [{
-        nombre: 'Nestor Brenes',
-        cedula: 'xxxxxxxx',
-        org: 'FZT',
-        area: 'CI'},
+        var contenido = $.getJSON('dist/assets/ton.json');
+        console.log();
+        registro = 
+        [{
+            nombre  : 'Nestor Brenes',
+            cedula  : 'xxxxxxxx',
+            org     : 'FZT',
+            area    : 'CI',
+            entrada : '8:00 am'
+        },
         {
-        nombre: 'Nestor Bonilla',
-        cedula: 'xxxxxxxx',
-        org: 'FZT',
-        area: 'CI'},
+            nombre  : 'Nestor Bonilla',
+            cedula  : 'xxxxxxxx',
+            org     : 'FZT',
+            area    : 'CI',
+            entrada : '8:00 am'
+        },
         {
-        nombre: 'Alice',
-        cedula: 'xxxxxxxx',
-        org: 'FZT',
-        area: 'SP'
-    }];
-
-    return reg;    
-},
+            nombre  : 'Alice',
+            cedula  : 'xxxxxxxx',
+            org     : 'FZT',
+            area    : 'SP',
+            entrada : '8:00 am'
+        }];
+        return registro;    
+    },
 
     actions: {
-        click: function(){
-            var name = $('#name').val();
-            var ced = $('#ced').val();
-            var org = $('#org').val();
-            var area = $('#area').val();
-            var obj = {
-                nombre : name,
-                cedula : ced,
-                org : org,
-                area : area
+        registrar_entrada: function(){
+            var nombre  = $('#nombre').val();
+            var cedula  = $('#cedula').val();
+            var org     = $('#org').val();
+            var area    = $('#area').val();
+            var entrada = moment().format('LT');
+
+            if ( !nombre || !cedula ) {
+                alert('Llene los campos obligatorios');
+                $('#nombre').css('border', '0.1rem solid red');
+                $('#cedula').css('border', '0.1rem solid red');
+            } else {
+                var obj     = {
+                    nombre  : nombre,
+                    cedula  : cedula,
+                    org     : org,
+                    area    : area,
+                    entrada : entrada
+                }
+
+                elementos.forEach(function(item) {
+                    $('#' + item).css('border', '0.1rem solid #d1d1d1');
+                    $('#' + item).val('');
+                });
+                
+                registro.addObject(obj);
             }
-            reg.addObject(obj);
-            console.log(obj);
-      }
+        },
+        registrar_entrada: function(event){
+            var salida = moment().format('LT');
+            console.log(this.get('id'));
+        }
     }
 });
 
---
+++ tests/unit/helpers/date-time-formater-test.js
@@ -0,0 +1,12 @@
+
+import { dateTimeFormater } from 'civ/helpers/date-time-formater';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | date time formater');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = dateTimeFormater([42]);
+  assert.ok(result);
+});
+
